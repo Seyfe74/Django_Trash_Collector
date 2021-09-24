a310ff6e728866200d666884f18673d243da8722
daisuket330 <88203620+daisuket330@users.noreply.github.com>
2021-09-24 17:17:50 -0400

    Update views.py
    completed filter method

--- trash_collector/employees/views.py
+++ trash_collector/employees/views.py
@@ -17,16 +17,29 @@ import calendar
 
 def index(request):
     # This line will get the Customer model from the other app, it can now be used to query the db for Customers
-
+Customer = apps.get_model('customers.Customer')
     user = request.user
     try:
-       logged_in_employee = Employee.objects.get(user=user)
+        logged_in_employee = Employee.objects.get(user=user)
     except:
-        employee = apps.get_model('employees.employee')
-        return render(request, 'employees/create.html')
+        return HttpResponseRedirect(reverse('employees:create'))
     print(user)
-    return render(request, 'employees/index.html')
-
+    
+    #TODO: moved the "todays pickup" to the index render so the pickups render right away in the index page
+    user = request.user
+    logged_in_employee = Employee.objects.get(user=user)
+    Customer = apps.get_model('customers.Customer')
+    today = date.today()
+    string_weekday = calendar.day_name[today.weekday()]
+    customers = Customer.objects.filter(zip_code=logged_in_employee.zip_code)
+    customer = []
+    for pick_ups in customers:
+        if (pick_ups.one_time_pickup == today or pick_ups.weekly_pickup == string_weekday) and logged_in_employee.zip_code == pick_ups.zip_code and (pick_ups.suspend_start < today and  today >= pick_ups.suspend_end or pick_ups.suspend_start > today and  today <= pick_ups.suspend_end) and pick_ups.suspend_start != '2000-01-01':
+            customer.append(pick_ups)
+    context = {
+        'customer' : customer
+        }
+    return render(request, 'employees/index.html', context)
 
 
 
@@ -58,3 +71,30 @@ def details(request):
             
         }
         return render(request, 'employees/details.html', context)
+    
+    def filter(request, day_of_week):
+    user = request.user
+    logged_in_employee = Employee.objects.get(user=user)
+    # is the above necessary? 
+    Customer = apps.get_model('customers.Customer')
+    customers = Customer.objects.filter(weekly_pickup = day_of_week)
+    customer = []
+    for filter_day in customers:
+        customer.append(filter_day)
+    context = {
+        'customer' : customer
+    }
+    return render(request, 'employees/filter.html', context)
+
+def charge_customer (request, charge, cust_id):
+    Customer = apps.get_model('customers.Customer')
+    charged_customer = Customer.objects.get(pk = cust_id)
+    if request.method == 'POST':
+        charged_customer.balance += charge
+        charged_customer.save()
+        return HttpResponseRedirect(reverse('employees:todays_pickups'))
+    else:
+        context = {
+            'charged_customer' : charged_customer
+        }
+        return render(request, 'employees/charge_customer.html', context)
